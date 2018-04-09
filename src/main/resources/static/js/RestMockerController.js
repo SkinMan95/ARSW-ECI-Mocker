8b7840ee666438ba8623efd89ad51681281be49d
Alejandro Anzola <anzola95avila@gmail.com>
2018-04-09 00:11:46 -0500

    borrador de comunicacion de topics con stomp y cambia canvas para que los tamaños sean definidos por el html (deben ser por el css)

--- src/main/resources/static/js/RestMockerController.js
+++ src/main/resources/static/js/RestMockerController.js
@@ -7,30 +7,61 @@ var RestMockerController = (function() {
 
     /* ==== VARIABLES ==== */
     var stompClient = undefined;
+    var stompIsConnected = undefined;
+    var subscribedTopics = undefined;
     
     /* ================ PUBLIC ================ */
     const TESTING = true;
 
     var init = function (callback) {
 	// TODO
+	subscribedTopics = [];
+	stompIsConnected = false;
     };
 
-    var registerToServer = function() {
-	// TODO
-    };
+    var registerToServer = function(userName, callback) {
+	console.info('Connecting to WS...');
+	var socket = new SockJS('/stompendpoint');
+	stompClient = Stomp.over(socket);
 
+	stompClient.connect({}, function (frame) {
+	    console.log(`Connected: ${frame}`);
+	    stompIsConnected = true;
+	});
+	
+	// TODO: axios realiza registro en el servidor
+    };	
+	
     var disconnectFromServer = function () {
-	// TODO
+	// TODO: axios informa al servidor para desconectarse
+	
+	console.assert(stompClient !== undefined);
+	stompClient.disconnect();
+	console.log('Disconnected');
+    };	
+	
+    var subscribeToTopic = function(topic, topicCallback) {
+	console.assert(topic instanceof String);
+
+	console.assert(stompClient !== undefined);
+	console.assert(stompIsConnected);
+	
+	stompClient.subscribe(topic, function (eventbody) {
+	    var payload = JSON.parse(eventbody.body);
+	    topicCallback(payload);
+	});
+
+	subscribedTopics.push(topic);
     };
 
-    var subscribeToTopic = function(topic) {
+    var publishInTopic = function (topic, payload) {
 	console.assert(topic instanceof String);
-	// TODO
-    };
-
-    var publishInTopic = function (topic) {
-	console.assert(topic instanceof String);
-	// TODO
+	console.assert(subscribedTopics.includes(topic));
+	console.assert(stompIsConnected);
+	
+	if (subscribedTopics.includes(topic) && stompIsConnected) {
+	    stompClient.send(topic, {}, JSON.stringify(payload));
+	}
     };
 
     return {
