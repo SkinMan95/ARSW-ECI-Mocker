1c8f0f959fcd820cad6da722f0b88efcf6532fa4
Camila Gomez <camilagomezr05@gmail.com>
2018-04-10 01:26:08 -0500

    Se corrigio la seleccion unica del color

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,9 +1,11 @@
 var username;
 var session;
 
+
 function Login(){
     username = document.getElementById("nombre").value;
     session = document.getElementById("session").value;
+
     if (username == ""|| session == "") {
         alert("DEBE LLENAR TODOS LOS CAMPOS");
     } 
