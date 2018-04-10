a3eb83c3a537b0440cd83c98259977b83edda5c6
Camila Gomez <camilagomezr05@gmail.com>
2018-04-10 01:06:03 -0500

    Verificación de llenado de los campos del login

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,1 +1,11 @@
-// TODO
+var username;
+var session;
+
+function Login(){
+    username = document.getElementById("nombre").value;
+    session = document.getElementById("session").value;
+    if (username == ""|| session == "") {
+        alert("DEBE LLENAR TODOS LOS CAMPOS");
+    } 
+    
+}
