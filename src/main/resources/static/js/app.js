cc6d41bc137070e6ef2ab6f958e377f1f30e61ee
Camila Gomez <camilagomezr05@gmail.com>
2018-04-10 02:02:28 -0500

    Acceso temporal al canvas desde el login

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,10 +2,10 @@ var username;
 var session;
 
 
+
 function Login(){
     username = document.getElementById("nombre").value;
     session = document.getElementById("session").value;
-
     if (username == ""|| session == "") {
         alert("DEBE LLENAR TODOS LOS CAMPOS");
     } 
