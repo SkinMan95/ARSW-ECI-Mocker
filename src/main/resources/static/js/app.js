ecc6ef239563a235d6485c3cf1e4f0f16615167f
unknown <2108746@labinfo.is.escuelaing.edu.co>
2018-05-02 18:10:17 -0500

    Funcion de color random hecha

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,13 +1,25 @@
 var username;
 var session;
+var color;
 
-
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 function Login(){
     username = document.getElementById("nombre").value;
     session = document.getElementById("session").value;
     if (username == ""|| session == "") {
         alert("DEBE LLENAR TODOS LOS CAMPOS");
-    } 
+    }
+    color = getRandomColor();
+    
+    alert('{ Nombre: ' + username + ' , ' + 'Color: ' + color + ' , ' + 'Sesion: ' + session + ' }');
+    
     
 }
