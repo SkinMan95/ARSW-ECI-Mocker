bc8eb5504563053fc0364be4eff9888d5f131298
Camila Gomez <camilagomezr05@gmail.com>
2018-05-20 10:32:00 -0500

    avance redis

--- src/main/java/edu/eci/arsw/ecimocker/logic/services/impl/RedisMockerServices.java
+++ src/main/java/edu/eci/arsw/ecimocker/logic/services/impl/RedisMockerServices.java
@@ -10,9 +10,18 @@ import edu.eci.arsw.ecimocker.entities.Session;
 import edu.eci.arsw.ecimocker.entities.User;
 import edu.eci.arsw.ecimocker.logic.services.MockerServices;
 import edu.eci.arsw.ecimocker.logic.services.MockerServicesException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.ConcurrentHashMap;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Response;
+import redis.clients.jedis.Transaction;
+import util.JedisUtil;
+
+
+
 
 /**
  *
@@ -23,14 +32,43 @@ public class RedisMockerServices implements MockerServices{
     public static final int TOKEN_BYTE_SIZE = 32; // 256 bits
     public static final String TOKEN_DICT = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    private static final AtomicInteger SESSIONS_IDS = new AtomicInteger(0);
-    private static final AtomicInteger USER_IDS = new AtomicInteger(0);
-
     private static final Random RANDOM = new Random();
+    
+    private static final int SESSIONS_IDS = 0;
+    
+   
+    /*public int addNewSession(String sessionName) throws MockerServicesException {
+        if (sessionName == null) {
+            throw new NullPointerException("sessionName es null");
+        }
+
+        if (sessions.containsKey(sessionName)) {
+            throw new MockerServicesException("Ya existe una sesion con el mismo nombre");
+        }
+
+        int id = SESSIONS_IDS.getAndAdd(1);
+        sessions.put(sessionName, new Session(id, sessionName));
+        sessionObjects.put(id, new ConcurrentHashMap<>());
+        return id;
+    }*/
 
     @Override
     public int addNewSession(String sessionName) throws MockerServicesException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Jedis jedis = JedisUtil.getPool().getResource();
+        
+        if (sessionName == null) {
+            throw new NullPointerException("sessionName es null");
+        }
+        
+        
+        int id = SESSIONS_IDS + 1;
+        
+        Transaction t = jedis.multi();
+        t.rpush(""+id, ""+sessionName);
+        List<Object> s = t.exec();
+        
+        throw new UnsupportedOperationException("Not supported yet.");
+        
     }
 
     @Override
